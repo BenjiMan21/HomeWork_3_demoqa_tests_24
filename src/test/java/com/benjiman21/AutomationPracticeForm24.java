5a86945f7c5e4d5fbdcb967cd7874c0d87963ac4
Alex Nikiforov <unleash211@gmail.com>
2023-12-25 10:46:07 +0400

    added check for (Thanks for submitting the form)

--- src/test/java/com/benjiman21/AutomationPracticeForm24.java
+++ src/test/java/com/benjiman21/AutomationPracticeForm24.java
@@ -21,15 +21,15 @@ public class AutomationPracticeForm24 {
     @Test
     void fillRegistrationForm() {
         open("/automation-practice-form");
-        executeJavaScript("$('footer').remove()");
         $("[id=firstName]").setValue("Alex");
         $("[id=lastName]").setValue("Nikiforov");
         $("[id=userEmail]").setValue("unleash21@mail.ru");
         $("[id=genterWrapper]").find(byText("Male")).click();
         $("[id=userNumber]").setValue("8999777665");
-        $("[id=dateOfBirthInput]").click(); $("[class=react-datepicker__month-select]").click(); $("[value='10']").click();
+        $("[id=dateOfBirthInput]").click();
+        $("[class=react-datepicker__month-select]").click(); $("[value='10']").click();
         $("[class=react-datepicker__year-select]").click(); $("[value='1989']").click();
-        $(byText("20")).click();
+        $("[class=react-datepicker__month]").find(byText("20")).click();
         $("[id=subjectsInput]").setValue("Eng"); $("[id=subjectsWrapper]").find(byText("English")).click();
         $("[id=subjectsInput]").setValue("Ma"); $("[id=subjectsWrapper]").find(byText("Maths")).click();
         $("[id=hobbiesWrapper]").find(byText("Sports")).click();
@@ -41,5 +41,7 @@ public class AutomationPracticeForm24 {
         $("[id=stateCity-wrapper]").find(byText("Select City")).click();
         $("[id=stateCity-wrapper]").find(byText("Delhi")).click();
         $("[id=submit]").click();
+
+        $("[class=modal-content]").shouldHave(text("Thanks for submitting the form"));
     }
 }
